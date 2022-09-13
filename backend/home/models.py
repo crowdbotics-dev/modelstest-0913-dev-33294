b5e11a5f94d494cc596799f6be9f9663424beb39
Crowdbotics <engineering@crowdbotics.com>
2022-09-13 18:18:18 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,17 @@
+from django.conf import settings
 from django.db import models
+
+
+class Pet(models.Model):
+    "Generated Model"
+    nickname = models.CharField(
+        max_length=100,
+    )
+    age = models.IntegerField()
+    breed = models.CharField(
+        max_length=100,
+    )
+    gender = models.CharField(
+        max_length=100,
+    )
+    description = models.TextField()
